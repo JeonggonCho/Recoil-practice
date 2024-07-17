9eccfc331c050bfddcbf1f630714a0df4edc9a01
조정곤 <wjdrhs62@gmail.com>
2024-07-17 20:52:15 +0900

    add: atom, useRecoilState, selector, useRecoilValue 사용한 간단한 앱

---
+++ simple-app/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import {atom, selector} from "recoil";
+import TextInput from "./components/TextInput";
+import CharacterCount from "./components/CharacterCount";
+
+export const textState = atom({
+  key: 'textState',
+  default: '',
+});
+
+export const charCountState = selector({
+  key: 'charCountState',
+  get: ({get}) => {
+    const text = get(textState);
+    return text.length;
+  }
+});
+
+function App() {
+  return (
+    <div style={{padding: 16}}>
+      <TextInput/>
+      <CharacterCount/>
+    </div>
+  );
+}
+
+export default App;
---
+++ simple-app/src/components/CharacterCount.js
@@ -0,0 +1,12 @@
+import {useRecoilValue} from "recoil";
+import {charCountState} from "../App";
+
+const CharacterCount = () => {
+  const count = useRecoilValue(charCountState);
+
+  return (
+    <div>Character Count: {count}</div>
+  );
+};
+
+export default CharacterCount;
